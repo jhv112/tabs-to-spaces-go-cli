1765027b3cf3a54113024b85d31ec7b127592ba0
jhv112 <117777422+jhv112@users.noreply.github.com>
2025-04-05 21:29:52 +0200

    Make argument parsing strict as opposed to lenient
    Just had a unpleasant experience of trying the program on its own
    repository, only to not add enough hyphens to the filter argument.
    Trashed the git diffs.
    Could only recover the repository by initing and pushing to GitHub.
    Somehow.
    Luckily.

--- main.go
+++ main.go
@@ -41,25 +41,21 @@ func parseArgs(argsStrings []string) (args Args, err error) {
 	// defaults
 	args = Args{regexp.MustCompile(".*"), ".", 4}
 
-	for _, arg := range argsStrings {
+	for _, arg := range argsStrings[1:] {
 		if strings.HasPrefix(arg, "--filter=") {
 			args.filter, err = regexp.Compile(arg[len("--filter="):])
 
 			if err != nil {
 				return Args{}, err
 			}
-		}
-
-		if strings.HasPrefix(arg, "--startdir=") {
+		} else if strings.HasPrefix(arg, "--startdir=") {
 			// from: https://stackoverflow.com/questions/45941821/a/64499397
 			args.startdir, err = filepath.Abs(arg[len("--startdir="):])
 
 			if err != nil {
 				return Args{}, err
 			}
-		}
-
-		if strings.HasPrefix(arg, "--tabsize=") {
+		} else if strings.HasPrefix(arg, "--tabsize=") {
 			parsedUint, err := strconv.ParseUint(arg[len("--tabsize="):], 10, 7)
 
 			if err != nil {
@@ -68,10 +64,10 @@ func parseArgs(argsStrings []string) (args Args, err error) {
 
 			// guaranteed to fit and be nonnegative
 			args.tabsize = int8(parsedUint)
-		}
-
-		if slices.Contains([]string{"-h", "--help"}, arg) {
+		} else if slices.Contains([]string{"-h", "--help"}, arg) {
 			return Args{}, fmt.Errorf("help")
+		} else {
+			return Args{}, fmt.Errorf("unrecognised argument: \"%s\"", arg)
 		}
 	}
 
